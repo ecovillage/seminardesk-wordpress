495e6428c1f7a8f7159cfd16c05b304b4a067c44
stdkr <erik@teschenhausen.com>
2020-04-29 12:33:01 -0300

    added webhook route with event.create and event.update; removed route dates and facilitators; rename methods create_event, create_event_date,  create_facilitator ...; extented error handeling

--- inc/Base/RestController.php
+++ inc/Base/RestController.php
@@ -32,11 +32,12 @@ class RestController extends WP_REST_Controller
      *
      * @var string
      */
-    protected $base_event, $base_date, $base_facilitator;
+    protected $base_webhook, $base_event, $base_date, $base_facilitator;
     
     function __construct()
     {
         $this->namespace = 'seminardesk/v1';
+        $this->base_webhook = 'webhooks';
         $this->base_event = 'events';
         $this->base_date = 'dates';
         $this->base_facilitator = 'facilitators';
@@ -56,6 +57,17 @@ class RestController extends WP_REST_Controller
      */
     public function register_routes()
     {
+        
+
+        // Webhook route registration for HTTP POSTs from SeminarDesk
+        register_rest_route($this->namespace, '/' . $this->base_webhook, array(
+            array(
+                'methods'             => WP_REST_Server::CREATABLE,
+                'callback'            => array($this, 'webhook_action'),
+                'permission_callback' => array($this, 'check_permissions'),
+                // 'args'                => $this->get_endpoint_args_for_item_schema( true ),
+            ),
+        ),);
 
         // General route registration
         register_rest_route($this->namespace, '/' . $this->base_event, array(
@@ -95,49 +107,6 @@ class RestController extends WP_REST_Controller
                 // 'args'                => $this->get_endpoint_args_for_item_schema( true ),
             ),
         ),);
-
-        // Date route registration
-        register_rest_route($this->namespace, '/' . $this->base_date . '/(?P<date_id>[0-9]+)', array(
-            array(
-                'methods'             => WP_REST_Server::READABLE,
-                'callback'            => array($this, 'get_item'),
-                'permission_callback' => array($this, 'check_permissions'),
-                // 'args'                => $this->get_endpoint_args_for_item_schema( true ),
-            ),
-            array(
-                'methods'             => WP_REST_Server::EDITABLE,
-                'callback'            => array($this, 'update_item'),
-                'permission_callback' => array($this, 'check_permissions'),
-                // 'args'                => $this->get_endpoint_args_for_item_schema( true ),
-            ),
-            array(
-                'methods'             => WP_REST_Server::DELETABLE,
-                'callback'            => array($this, 'delete_item'),
-                'permission_callback' => array($this, 'check_permissions'),
-                // 'args'                => $this->get_endpoint_args_for_item_schema( true ),
-            ),
-        ),);
-        // Register the individual object route for events
-        register_rest_route($this->namespace, '/' . $this->base_facilitator . '/(?P<facilitator_id>[a-z0-9]+)', array(
-            array(
-                'methods'             => WP_REST_Server::READABLE,
-                'callback'            => array($this, 'get_facilitator'),
-                'permission_callback' => array($this, 'check_permissions'),
-                // 'args'                => $this->get_endpoint_args_for_item_schema( true ),
-            ),
-            array(
-                'methods'             => WP_REST_Server::EDITABLE,
-                'callback'            => array($this, 'update_facilitator'),
-                'permission_callback' => array($this, 'check_permissions'),
-                // 'args'                => $this->get_endpoint_args_for_item_schema( true ),
-            ),
-            array(
-                'methods'             => WP_REST_Server::DELETABLE,
-                'callback'            => array($this, 'delete_facilitator'),
-                'permission_callback' => array($this, 'check_permissions'),
-                // 'args'                => $this->get_endpoint_args_for_item_schema( true ),
-            ),
-        ),);
     }
 
     /**
@@ -155,6 +124,56 @@ class RestController extends WP_REST_Controller
     public function get_test($request)
     {
         return new WP_Error('not_implemented', 'endpoint not yet implemented', array('status' => 400));
+    }
+
+    /**
+     * Process HTTP POSTs from SeminarDesk
+     *
+     * @param WP_Request $request
+     * @return WP_REST_Response|WP_Error
+     */
+    public function webhook_action($request)
+    {
+        $request_json = (array)$request->get_json_params(); // complete JSON data of the request#
+        switch ($request_json['action']) {
+            case 'event.create':
+                $response = $this->event_create($request_json);
+                break;
+            case 'event.update':
+                $response = $this->event_update($request_json);
+                break;
+            case 'event.delete':
+                $response = $this->event_delete($request_json);
+                break;
+            case 'eventDate.create':
+                $response = $this->event_date_create($request_json);
+                break;
+            case 'eventDate.update':
+                $response = $this->event_date_update($request_json);
+                break;
+            case 'eventDate.delete':
+                $response = $this->event_date_delete($request_json);
+                break;
+            case 'facilitator.create':
+                $response = $this->facilitator_create($request_json);
+                break;
+            case 'facilitator.update':
+                $response = $this->facilitator_update($request_json);
+                break;
+            case 'facilitator.delete':
+                $response = $this->facilitator_delete($request_json);
+                break;
+            default:  
+                $response = new WP_Error('not_supported', 'action ' . $request_json['action'] . ' not supported', array('status' => 400));
+        }
+
+        // return error if $response is of type WP_Error
+        if ( is_wp_error($response) )
+        {
+            return $response;
+        }
+        
+        return new WP_REST_Response($response, 201);
     }
 
     /**
@@ -198,24 +217,11 @@ class RestController extends WP_REST_Controller
     }
 
     /**
-     * Update event via event id
-     *
-     * @param WP_REST_Request $request
-     * @return WP_REST_Response|WP_Error
-     */
-    public function update_event($request)
-    {
-
-
-        $get_event = $this->get_event($request);
-
-        $this->create_event($request);
-
-
-
-        return new WP_Error('not_implemented', 'Update event method not implemented yet', array('status' => 400));
-    }
-
+    * Get all events
+    *
+    * @param WP_REST_Request $request
+    * @return WP_REST_Response|WP_Error
+    */
     public function get_items($request)
     {
         $args = [
@@ -280,7 +286,7 @@ class RestController extends WP_REST_Controller
         switch ($request_json['action']) {
             case 'event.create':
                 // echo 'create new event';
-                $new_item = $this->create_event($request_json);
+                $new_item = $this->event_create($request_json);
                 $type = is_wp_error($new_item);
                 if ( is_wp_error($new_item) )
                 {
@@ -288,14 +294,14 @@ class RestController extends WP_REST_Controller
                 }
                 break;
             case 'eventDate.create':
-                $new_item = $this->create_event_date($request_json);
+                $new_item = $this->event_date_create($request_json);
                 if ( is_wp_error($new_item) )
                 {
                     return $new_item;
                 }
                 break;
             case 'facilitator.create':
-                $new_item = $this->create_facilitator($request_json);
+                $new_item = $this->facilitator_create($request_json);
                 if ( is_wp_error($new_item) )
                 {
                     return $new_item;
@@ -307,11 +313,17 @@ class RestController extends WP_REST_Controller
         return new WP_REST_Response($new_item, 201);
     }
 
-    public function create_event($request_json)
+    /**
+     * Create event via webhook from SeminarDesk
+     *
+     * @param Array $request_json 
+     * @return WP_REST_Response|WP_Error
+     */
+    public function event_create($request_json)
     {
-        $payload = $request_json['payload']; // payload of the request in JSON
+        $payload = (array)$request_json['payload']; // payload of the request in JSON
 
-        // if event_id already exists don't create new event and return error message
+        // checks if event_id already exists don't create new event and return error message
         // $posts = get_posts([
         //     'numberposts' => -1, // all events
         //     'post_type' => 'sd_event',
@@ -345,7 +357,12 @@ class RestController extends WP_REST_Controller
         //$event_attr = wp_slash($event_attr);
 
         // create new event in the WordPress database
-        $event_id = wp_insert_post($event_attr);
+        $post_id = wp_insert_post(wp_slash($event_attr), true);
+
+        // return error if $post_id is of type WP_Error
+        if (is_wp_error($post_id)){
+            return $post_id;
+        }
 
         // update metadata of CPT sd_event
         // $event_update = [
@@ -368,19 +385,146 @@ class RestController extends WP_REST_Controller
                 'tmp_name' => download_url( $payload['teaserPictureUrl']['0']['value'] ),
             ]
         );
-        set_post_thumbnail($event_id, $img_id);
+        set_post_thumbnail($post_id, $img_id);
 
-        $event = get_post($event_id);
+        $event = get_post($post_id);
         return $event;
     }
 
-    public function create_event_date($data)
+    /**
+     * Update event via webhook from SeminarDesk
+     *
+     * @param Array $request_json
+     * @return WP_REST_Response|WP_Error
+     */
+    public function event_update($request_json)
+    {
+        $payload = (array)$request_json['payload']; // payload of the request in JSON
+        
+        // checks if event_id exists and get post id in WordPress
+        $events = get_posts([
+            'numberposts' => -1, // all events
+            'post_type' => 'sd_event',
+            'post_status' => 'any',
+        ],);
+        foreach ($events as $current) {
+            if ( $current->event_id == $payload['id']){
+                $post_id = $current->ID;
+                break;
+            }
+        }
+
+        if ( !isset($post_id) ){
+            return new WP_Error('no_event', 'Event not updated. Event ID ' . $payload['id'] . ' does not exists', array('status' => 404));
+        }
+
+        // define metadata of the new sd_event
+        $meta = [
+            'event_id' =>  $payload['id'],
+            'json_dump' => $request_json,
+        ];
+
+        // define attributes of the new sd_event using $payload of the 
+        $event_attr = [
+            'ID'            => $post_id,
+            'post_type'     => 'sd_event',
+            'post_title'    =>  $payload['title']['0']['value'],
+            //'post_title'    =>  wp_strip_all_tags($payload['title']['0']['value']), // remove HTML, JavaScript, or PHP tags from the title of the post
+            'post_content'  => $payload['description']['0']['value'],
+            // 'post_content'  => wp_strip_all_tags($payload['description']['0']['value']),
+            'post_excerpt'  => $payload['teaser']['0']['value'],
+            'post_author'   => get_current_user_id(),
+            'post_status'   => 'publish',
+            'meta_input'    => $meta,
+        ];
+
+        // Update event data in the database
+        $post_id = wp_update_post( wp_slash($event_attr), true );
+
+        // return error if $post_id is of type WP_Error
+        if (is_wp_error($post_id)){
+            return $post_id;
+        }
+
+        // get updated event via post id and return it
+        $event = get_post($post_id);
+        return $event;
+    }
+
+    /**
+     * Delete event via event id
+     *
+     * @param Array $request_json
+     * @return WP_REST_Response|WP_Error
+     */
+    public function event_delete($request_json)
+    {
+        return new WP_Error('not_implemented', 'Update event method not implemented yet', array('status' => 400));
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param Array $request_json
+     * @return WP_REST_Response|WP_Error
+     */
+    public function event_date_create($request_json)
     {   
-        return new WP_Error('not_implemented', 'action ' . $data['action'] . ' not implemented yet', array('status' => 404));
+        return new WP_Error('not_implemented', 'action ' . $request_json['action'] . ' not implemented yet', array('status' => 404));
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param Array $request_json
+     * @return WP_REST_Response|WP_Error
+     */
+    public function event_date_update($request_json)
+    {   
+        return new WP_Error('not_implemented', 'action ' . $request_json['action'] . ' not implemented yet', array('status' => 404));
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param Array $request_json
+     * @return WP_REST_Response|WP_Error
+     */
+    public function event_date_delete($request_json)
+    {   
+        return new WP_Error('not_implemented', 'action ' . $request_json['action'] . ' not implemented yet', array('status' => 404));
     }
     
-    public function create_facilitator($data)
+    /**
+     * Undocumented function
+     *
+     * @param Array $request_json
+     * @return WP_REST_Response|WP_Error
+     */
+    public function facilitator_create($request_json)
     {
-        return new WP_Error('not_implemented', 'action ' . $data['action'] . ' not implemented yet', array('status' => 404));
+        return new WP_Error('not_implemented', 'action ' . $request_json['action'] . ' not implemented yet', array('status' => 404));
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param Array $request_json
+     * @return WP_REST_Response|WP_Error
+     */
+    public function facilitator_update($request_json)
+    {
+        return new WP_Error('not_implemented', 'action ' . $request_json['action'] . ' not implemented yet', array('status' => 404));
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param Array $request_json
+     * @return WP_REST_Response|WP_Error
+     */
+    public function facilitator_delete($request_json)
+    {
+        return new WP_Error('not_implemented', 'action ' . $request_json['action'] . ' not implemented yet', array('status' => 404));
     }
 }
