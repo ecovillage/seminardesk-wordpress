39f858e7d20f9fa2474f41601052901c53cfc1bf
stdkr <erik@teschenhausen.com>
2020-05-17 18:55:56 -0300

    minor change taxonomy: - change slug to schedule - make taxonomy hierarchical .../schedule/2020/01

--- inc/Base/TaxonomyController.php
+++ inc/Base/TaxonomyController.php
@@ -14,7 +14,7 @@ class TaxonomyController
     public $name_lower;
     public $names = 'Dates';
     public $names_lower;
-    // public $slug = 'schedule';
+    public $slug = 'schedule';
 
     // public $taxonomy = array(
     //     'names' => 'Dates',
@@ -57,7 +57,8 @@ class TaxonomyController
             'show_in_rest'      => true, // http://localhost/wpsdp/wp-json/wp/v2/dates
             //'rest_base'         => 'txn',
             'rewrite'           => array( 
-                'slug' => $this->names_lower, 
+                'slug' => $this->slug, 
+                'hierarchical' => true,
                 'with_front' => false, 
             ),
         );
